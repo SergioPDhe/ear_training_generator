9bb1e5505bd43e98e5c4f922d20c75c9bf6126ee
Sergio Ponde Dhelomme <sergio.p.dhe@gmail.com>
2023-07-09 11:55:42 -0400

    midi_generator working

---
+++ src/midi_generator.py
@@ -0,0 +1,33 @@
+import json
+import os
+from pathlib import Path
+import numpy as np
+import mido
+from mido import MidiFile, Message, MidiTrack
+
+home_dir = Path(os.getcwd())
+home_dir = str(home_dir.parent.absolute())
+
+f = open(home_dir + "/generator_settings.json")
+data = json.load(f)
+f.close()
+del f
+
+notes = np.genfromtxt("notes.csv", delimiter=',')
+notes = notes.astype(int)
+
+mid = MidiFile()
+track = MidiTrack()
+already_added = []
+
+
+for i,pair in enumerate(notes):         # Get the index and the note. Array must be int notes             
+
+    track.append(Message('note_on',note = pair[0], velocity = 120,time = 5000))
+    track.append(Message('note_on',note = pair[1], velocity = 120,time = 1000))
+    
+    track.append(Message('note_on',note = pair[0], velocity = 0,time = 2000))
+    track.append(Message('note_on',note = pair[1], velocity = 0,time = 0))
+    
+mid.tracks.append(track)
+mid.save("notes.mid")
